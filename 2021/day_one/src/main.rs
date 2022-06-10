d3325bb15a864d37233603f53b3413ab2f7fd1bb
Teo Oliver <teo.liz.oli@gmail.com>
2022-06-10 20:37:46 +0100

    feat: done

--- 2021/day_one/src/main.rs
+++ 2021/day_one/src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("Hello, world!");
+    let report: Vec<u32> = vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263];
+
+    let mut depth_increments = 0;
+
+    let mut previous_number = &report[0];
+
+    for number in report.iter() {
+        if number > previous_number {
+            depth_increments += 1;
+        }
+        previous_number = number;
+    }
+
+    println!("How many measurements are larger than the previous measurement?");
+    println!("{}", depth_increments);
 }
