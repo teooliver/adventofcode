20c9dbc094c18be1f942371002e5f02380fb9760
Teo Oliver <teo.liz.oli@gmail.com>
2022-06-11 10:30:08 +0100

    feat: finish day 1 part b

---
+++ aoc_2021/src/day1.rs
@@ -0,0 +1,54 @@
+fn part_a(input: &str) -> i64 {
+    let report: Vec<i64> = input
+        .trim()
+        .split('\n')
+        .map(|line| line.parse::<i64>().unwrap())
+        .collect();
+
+    let mut depth_increments = 0;
+
+    let mut previous_number = &report[0];
+
+    for number in report.iter() {
+        if number > previous_number {
+            depth_increments += 1;
+        }
+        previous_number = number;
+    }
+
+    return depth_increments;
+}
+
+pub fn part_b(input: &str) -> i64 {
+    let report: Vec<i64> = input
+        .trim()
+        .split('\n')
+        .map(|line| line.parse::<i64>().unwrap())
+        .collect();
+
+    let mut depth_increments = 0;
+    let mut previous_window = 0;
+    let mut current_window = 0;
+
+    for i in 3..report.len() {
+        current_window = report[i] + report[i - 1] + report[i - 2];
+        previous_window = report[i - 1] + report[i - 2] + report[i - 3];
+
+        if current_window > previous_window {
+            depth_increments += 1;
+        }
+        previous_window = current_window;
+    }
+
+    depth_increments
+}
+
+#[cfg(test)]
+mod day_one_tests {
+    use super::{part_a, part_b};
+    #[test]
+    fn test_day_one() {
+        assert_eq!(part_a(include_str!("input.txt")), 1226);
+        assert_eq!(part_b(include_str!("input.txt")), 1252);
+    }
+}
---
+++ aoc_2021/src/main.rs
@@ -0,0 +1,5 @@
+mod day1;
+
+fn main() {
+    day1::part_b(include_str!("input.txt"));
+}
