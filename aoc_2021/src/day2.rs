e2535e0481956c3bc17ba533083fcef63cf56c99
Teo Oliver <teo.liz.oli@gmail.com>
2022-06-11 13:35:21 +0100

    feat: finish day 2 part b

--- aoc_2021/src/day2.rs
+++ aoc_2021/src/day2.rs
@@ -43,11 +43,38 @@ pub fn part_a(input: &str) -> i64 {
     end_position.x * end_position.y
 }
 
+pub fn part_b(input: &str) -> i64 {
+    let mut end_position = EndPosition { x: 0, y: 0 };
+    let mut aim = 0;
+
+    let lines: Vec<&str> = input.trim().split('\n').collect();
+
+    for line in lines {
+        let direction: Vec<&str> = line.split(" ").collect();
+
+        if direction[0] == "forward" {
+            end_position.x = end_position.x + direction[1].parse::<i64>().unwrap();
+            end_position.y = end_position.y + (aim * direction[1].parse::<i64>().unwrap());
+        }
+
+        if direction[0] == "down" {
+            aim = aim + direction[1].parse::<i64>().unwrap();
+        }
+
+        if direction[0] == "up" {
+            aim = aim - direction[1].parse::<i64>().unwrap();
+        }
+    }
+
+    end_position.x * end_position.y
+}
+
 #[cfg(test)]
-mod day_one_tests {
-    use super::part_a;
+mod day_two_tests {
+    use super::{part_a, part_b};
     #[test]
     fn test_day_one() {
         assert_eq!(part_a(include_str!("day2_input.txt")), 1670340);
+        assert_eq!(part_b(include_str!("day2_input.txt")), 1954293920);
     }
 }
