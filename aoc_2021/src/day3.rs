c8704c93d04ec912d527006de8bddeb5473aae4e
Teo Oliver <teo.liz.oli@gmail.com>
2022-06-11 14:12:25 +0100

    chore: prepare for day3

---
+++ aoc_2021/src/day3.rs
@@ -0,0 +1,13 @@
+pub fn part_a(input: &str) -> i64 {}
+
+pub fn part_b(input: &str) -> i64 {}
+
+#[cfg(test)]
+mod day_two_tests {
+    use super::{part_a, part_b};
+    #[test]
+    fn test_day_one() {
+        // assert_eq!(part_a(include_str!("day3_input.txt")), 1670340);
+        // assert_eq!(part_b(include_str!("day3_input.txt")), 1954293920);
+    }
+}
