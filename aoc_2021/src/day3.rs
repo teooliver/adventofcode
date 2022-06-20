71cafc7f87859e8dfe3facbff3aa1ec918d8190a
Teo Oliver <teo.liz.oli@gmail.com>
2022-06-20 22:32:24 +0100

    feat: finish day 3

--- aoc_2021/src/day3.rs
+++ aoc_2021/src/day3.rs
@@ -1,7 +1,7 @@
 pub fn part_a(input: &str) -> i64 {
     let lines: Vec<&str> = input.trim().split('\n').collect();
 
-    let mut values_array: Vec<u32> = vec![0; 12];
+    let mut values_array: Vec<usize> = vec![0; 12];
 
     let mut gama_rate_array: Vec<u32> = vec![0; 12];
     let mut epsilon_rate_array: Vec<u32> = vec![0; 12];
@@ -15,7 +15,7 @@ pub fn part_a(input: &str) -> i64 {
     }
 
     for (i, value) in values_array.iter().enumerate() {
-        let x: u32 = (lines.len().clone() / 2).try_into().unwrap();
+        let x: usize = lines.len().clone() / 2;
         if value > &x {
             gama_rate_array[i] = 1;
             epsilon_rate_array[i] = 0;
@@ -75,17 +75,71 @@ pub fn part_a_alternative(input: &str) -> i64 {
     gama * epsilon
 }
 
-// pub fn part_b(input: &str) -> i64 {}
+fn get_element(input: &str, f: fn(usize, usize) -> bool) -> usize {
+    // For each digit, add the most common one, then filter the lines
+    // progressively against each push:
+    //  "0"
+    //  "01"
+    //  "010"
+    //  "0101"
+    //  "01011"
+    //  "010110"
+    //  "0101101"
+    //  "01011011"
+    //  "010110110"
+    //  "0101101100"
+    //  "01011011001"
+    //  "010110110011"
+
+    let mut most_common_bits = String::new();
+
+    for digit in 0..12 {
+        let filtered: Vec<_> = input
+            .lines()
+            .filter(|line| line.starts_with(&most_common_bits))
+            .collect();
+
+        if filtered.len() == 1 {
+            break;
+        }
+
+        let count1 = filtered
+            .iter()
+            .map(|&line| line.chars().nth(digit).unwrap())
+            .filter(|&c| c == '1')
+            .count();
+
+        most_common_bits.push(if f(count1, filtered.len() - count1) {
+            '1'
+        } else {
+            '0'
+        });
+        println!("Filtered {:?}", most_common_bits);
+    }
+
+    let filtered: Vec<_> = input
+        .lines()
+        .filter(|line| line.starts_with(&most_common_bits))
+        .collect();
+
+    usize::from_str_radix(filtered[0], 2).unwrap()
+}
+
+pub fn part_b(input: &str) -> usize {
+    let oxygen = get_element(input, |count1, count0| count1 >= count0);
+    let co2 = get_element(input, |count1, count0| count1 < count0);
+    oxygen * co2
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::day3::part_a_alternative;
+    use crate::day3::{part_a_alternative, part_b};
 
     use super::part_a;
     #[test]
     fn test_day_3() {
         assert_eq!(part_a(include_str!("day3_input.txt")), 845186);
         assert_eq!(part_a_alternative(include_str!("day3_input.txt")), 845186);
-        // assert_eq!(part_b(include_str!("day3_input.txt")), 1954293920);
+        assert_eq!(part_b(include_str!("day3_input.txt")), 4636702);
     }
 }
--- aoc_2021/src/main.rs
+++ aoc_2021/src/main.rs
@@ -3,7 +3,18 @@ mod day2;
 mod day3;
 
 fn main() {
+    fn playground() {
+        let a: Vec<&str> = vec!["0", "1", "2", "3", "4", "5", "6"];
+        let b: Vec<&str> = vec!["0", "4", "5", "6"];
+
+        let mut iter: Vec<_> = a.iter().filter(|x| b.contains(x)).collect(); // both & and *
+
+        println!("=====> {:?}", iter);
+    }
+    // playground();
+
     // day1::part_b(include_str!("day1_input.txt"));
     // day2::part_b(include_str!("day2_input.txt"));
-    day3::part_a(include_str!("day3_input.txt"));
+    // day3::part_a(include_str!("day3_input.txt"));
+    day3::part_b(include_str!("day3_input.txt"));
 }
